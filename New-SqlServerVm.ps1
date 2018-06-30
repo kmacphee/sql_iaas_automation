d10a3164243014b242198525315f7681c7d21730
Kirk MacPhee <kirk@anchorloop.com>
2018-06-30 22:26:14 +0100

    Iron out issues

--- New-SqlServerVm.ps1
+++ New-SqlServerVm.ps1
@@ -17,28 +17,30 @@ $vnet = $resourceGroup | New-AzureRmVirtualNetwork -Name 'sqlserver-vnet' -Addre
 $pip = $resourceGroup | New-AzureRmPublicIpAddress -AllocationMethod Static -IdleTimeoutInMinutes 4 -Name 'sqlserver-pip'
 
 # Create network security policy
-$sqlRule = New-AzureRmNetworkSecurityRuleConfig -Name 'sqlrule' -Protocol 'Tcp' -Direction Inbound -Priority 1000 `
-    -SourceAddressPrefix $SqlClientIpAddress -SourcePortRange 1433 -DestinationAddressPrefix * -DestinationPortRange 1433 -Access 'Allow'
-$nsg = $resourceGroup | New-AzureRmNetworkSecurityGroup -Name $NsgName -SecurityRules @($sqlRule)
+$rdpRule = New-AzureRmNetworkSecurityRuleConfig -Name 'AllowRdp' -Protocol 'Tcp' -Direction Inbound -Priority 1000 `
+    -SourceAddressPrefix $SqlClientIpAddress -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389 -Access 'Allow'
+$sqlRule = New-AzureRmNetworkSecurityRuleConfig -Name 'AllowSql' -Protocol 'Tcp' -Direction Inbound -Priority 1001 `
+    -SourceAddressPrefix $SqlClientIpAddress -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 1433 -Access 'Allow'
+$nsg = $resourceGroup | New-AzureRmNetworkSecurityGroup -Name 'sqlserver-nsg' -SecurityRules @($rdpRule, $sqlRule)
 
 # Create network interface
 $nic = $resourceGroup | New-AzureRmNetworkInterface -Name 'sqlserver-nic' -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id `
    -NetworkSecurityGroupId $nsg.Id
 
 # Create storage account and upload SQL Server configuration script to blob storage
-$storageAccount = $resourceGroup | New-AzureRmStorageAccount -AccountName "anchorloopsqlvm" -SkuName 'Standard_LRS' -Kind 'Storage'
+$storageAccount = $resourceGroup | New-AzureRmStorageAccount -StorageAccountName "anchorloopsqlvm" -SkuName 'Standard_LRS' -Kind 'Storage'
 $storageAccountKey = ($storageAccount | Get-AzureRmStorageAccountKey).Value[0]
-$storageContext = $storageAccount | New-AzureStorageContext -StorageAccountKey $storageAccountKey
+$storageContext = New-AzureStorageContext -StorageAccountName $storageAccount.StorageAccountName -StorageAccountKey $storageAccountKey
 $storageContainer = $storageContext | New-AzureStorageContainer -Name 'sqlserver' -Permission 'Blob'
 $customScript = 'Set-SqlServerConfig.ps1'
-Set-AzureStorageBlobContent -File "./$customScript" -Container $storageContainer -Blob $customScript -Context $storageContext
+Set-AzureStorageBlobContent -File ".\$customScript" -Container $storageContainer.Name -Blob $customScript -Context $storageContext
 
 # Create admin credential
 $securePassword = ConvertTo-SecureString -String $VmAdminPassword -AsPlainText -Force
 $credential = New-Object -TypeName 'System.Management.Automation.PSCredential' -ArgumentList $VmAdminUsername, $securePassword
 
 # Create virtual machine configuration
-$vmConfig = New-AzureRmVMConfig -VMName 'sqlserver' -VMSize 'Standard_D2' | `
+$vmConfig = New-AzureRmVMConfig -VMName 'sqlserver' -VMSize 'Standard_D2_v2' | `
    Set-AzureRmVMOperatingSystem -Windows -ComputerName 'sqlserver' -Credential $credential -ProvisionVMAgent -EnableAutoUpdate | `
    Set-AzureRmVMSourceImage -PublisherName "MicrosoftSQLServer" -Offer "SQL2017-WS2016" -Skus "SQLDEV" -Version "latest" | `
    Add-AzureRmVMNetworkInterface -Id $nic.Id
@@ -47,5 +49,7 @@ $vmConfig = New-AzureRmVMConfig -VMName 'sqlserver' -VMSize 'Standard_D2' | `
 $vm = $resourceGroup | New-AzureRmVM -VM $vmConfig
 
 # Set VM custom script extension to configure SQL Server
-Set-AzureRmVMCustomScriptExtension -VM $vm -StorageAccountName $storageAccount -ContainerName $storageContainer `
-    -FileName $customScript -Run $customScript | Update-AzureRmVM
+$resourceGroup | `
+    Set-AzureRmVMCustomScriptExtension -Name 'SetSqlServerConfig' -VMName 'sqlserver' -StorageAccountName $storageAccount.StorageAccountName -ContainerName $storageContainer.Name -FileName $customScript `
+        -Argument "-VmAdminUsername $VmAdminUsername -VmAdminPassword $VmAdminPassword -SqlLoginUsername $SqlLoginUsername -SqlLoginPassword $SqlLoginPassword -SqlClientIpAddress $SqlClientIpAddress" | `
+    Update-AzureRmVM
--- Set-SqlServerConfig.ps1
+++ Set-SqlServerConfig.ps1
@@ -6,20 +6,24 @@ Param (
     [Parameter(Mandatory=$true)] [string] $SqlClientIpAddress
 )
 
+Install-PackageProvider -Name 'NuGet' -RequiredVersion '2.8.5.201' -Force
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy 'Trusted'
-Install-Module -Name 'SqlServer'
+Install-Module -Name 'SqlServer' -AllowClobber -Force
 
 # This script will be executed by the custom script extension in the SYSTEM user context. We need to talk to
 # SQL Server with the VM administrator account, which is the default SQL administrator in the marketplace image.
 # This PSCredential will allow us to act as the VM administrator.
 $secureVmAdminPassword = ConvertTo-SecureString -String $VmAdminPassword -AsPlainText -Force
-$vmAdminCredential = New-Object -TypeName 'PSCredential' -ArgumentList $VmAdminUsername, $secureVmAdminPassword
+$vmAdminCredential = New-Object -TypeName 'PSCredential' -ArgumentList "$env:ComputerName\$VmAdminUsername", $secureVmAdminPassword
 
 # Enable mixed mode authentication (service restart required). By default the marketplace image is Windows
-# authentication only.
-Invoke-Sqlcmd -ServerInstance 'localhost' -Database 'master' -Credential $vmAdminCredential `
-    -Query "EXEC xp_instance_regwrite N’HKEY_LOCAL_MACHINE’, N'Software\Microsoft\MSSQLServer\MSSQLServer', N'LoginMode', REG_DWORD, 2"
-Restart-Service -Name 'MSSQLServer'
+# authentication only. We need to do this in the execution context of the VM administrator until after we
+# activate mixed mode authentication.
+Invoke-Command -Credential $vmAdminCredential -ScriptBlock {
+    Invoke-Sqlcmd -ServerInstance 'localhost' -Database 'master' `
+        -Query "EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'LoginMode', REG_DWORD, 2"
+    Restart-Service -Name 'MSSQLServer'
+}
 
 # Add SQL login
 $secureSqlLoginPassword = ConvertTo-SecureString -String $SqlLoginPassword -AsPlainText -Force
@@ -27,17 +31,17 @@ $sqlLoginCredential = New-Object -TypeName 'PSCredential' -ArgumentList $SqlLogi
 Add-SqlLogin -ServerInstance 'localhost' -Credential $vmAdminCredential -LoginPSCredential $sqlLoginCredential
 
 # Configure Windows firewall rules. These only allow traffic from the specified client IP address.
-New-NetFirewallRule -DisplayName 'SQL Server' -Direction 'Inbound' –Protocol 'TCP' –LocalPort 1433 `
+New-NetFirewallRule -DisplayName 'SQL Server' -Direction 'Inbound' -Protocol 'TCP' -LocalPort 1433 `
     -RemoteAddress @($SqlClientIpAddress) -Action 'Allow'
-New-NetFirewallRule -DisplayName 'SQL Admin Connection' -Direction 'Inbound' –Protocol 'TCP' –LocalPort 1434 `
+New-NetFirewallRule -DisplayName 'SQL Admin Connection' -Direction 'Inbound' -Protocol 'TCP' -LocalPort 1434 `
     -RemoteAddress @($SqlClientIpAddress) -Action 'Allow'
-New-NetFirewallRule -DisplayName 'SQL Database Management' -Direction 'Inbound' –Protocol 'UDP' –LocalPort 1434 `
+New-NetFirewallRule -DisplayName 'SQL Database Management' -Direction 'Inbound' -Protocol 'UDP' -LocalPort 1434 `
     -RemoteAddress @($SqlClientIpAddress) -Action 'Allow'
-New-NetFirewallRule -DisplayName 'SQL Service Broker' -Direction 'Inbound' –Protocol 'TCP' –LocalPort 4022 `
+New-NetFirewallRule -DisplayName 'SQL Service Broker' -Direction 'Inbound' -Protocol 'TCP' -LocalPort 4022 `
     -RemoteAddress @($SqlClientIpAddress) -Action 'Allow'
-New-NetFirewallRule -DisplayName 'SQL Debugger/RPC' -Direction 'Inbound' –Protocol 'TCP' –LocalPort 135 `
+New-NetFirewallRule -DisplayName 'SQL Debugger/RPC' -Direction 'Inbound' -Protocol 'TCP' -LocalPort 135 `
     -RemoteAddress @($SqlClientIpAddress) -Action 'Allow'
-New-NetFirewallRule -DisplayName 'SQL Analysis Services' -Direction 'Inbound' –Protocol 'TCP' –LocalPort 2383 `
+New-NetFirewallRule -DisplayName 'SQL Analysis Services' -Direction 'Inbound' -Protocol 'TCP' -LocalPort 2383 `
     -RemoteAddress @($SqlClientIpAddress) -Action 'Allow'
-New-NetFirewallRule -DisplayName 'SQL Browser' -Direction 'Inbound' –Protocol 'TCP' –LocalPort 2382 `
+New-NetFirewallRule -DisplayName 'SQL Browser' -Direction 'Inbound' -Protocol 'TCP' -LocalPort 2382 `
     -RemoteAddress @($SqlClientIpAddress) -Action 'Allow'
